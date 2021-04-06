70f156a470be46a1e007a407c7ec28438f138d1c
Alex Morozov <igroman-x@list.ru>
2021-04-06 18:42:07 +0300

    Netology active post v0.01

--- app/src/main/java/ru/netology/trialapplication/MainActivity.kt
+++ app/src/main/java/ru/netology/trialapplication/MainActivity.kt
@@ -1,11 +1,16 @@
 package ru.netology.trialapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.trialapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(LayoutInflater)
+        setContentView(binding.root)
+
     }
 }
