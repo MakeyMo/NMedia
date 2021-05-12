c28c59e96ec3980f67dc6a361f0a53941a1452ae
Alex Morozov <igroman-x@list.ru>
2021-05-12 19:39:48 +0300

    Netology active post v2.1

--- app/src/main/java/ru/netology/trialapplication/MainActivity.kt
+++ app/src/main/java/ru/netology/trialapplication/MainActivity.kt
@@ -18,22 +18,30 @@ class MainActivity : AppCompatActivity() {
                 content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растем сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остается с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку перемен ➝ http://netolo.gy/fyb",
                 likedByMe = false,
                 likesCount = 10,
-                sharesCount = 5
+                sharesCount = 5,
+                viewsCount = 5
         )
         with(binding) {
             txtAuthor.text = post.author
             txtPublished.text = post.published
             txtContent.text = post.content
             txtLikesCount.text = post.likesCount.toString()
-//            if (post.likedByMe) {
-//                imgLike?.setImageResource(R.drawable.ic_liked_24)
-//            }
+            txtSharesCount.text = post.sharesCount.toString()
+            txtViewsCount.text = post.viewsCount.toString()
+            if (post.likedByMe) {
+                imgLike?.setImageResource(R.drawable.ic_liked_24)
+            }
             imgLike?.setOnClickListener {
                 post.likedByMe = !post.likedByMe
                 imgLike.setImageResource(
                         if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
                 )
-                if (post.likedByMe) post.likesCount += 1 else post.likesCount
+                if (post.likedByMe) post.likesCount += 1 else post.likesCount -= 1
+                txtLikesCount.text = post.likesCountSimpleView(post.likesCount)
+            }
+            imgShare?.setOnClickListener {
+                post.sharesCount += 1
+                txtSharesCount.text = post.sharesCountSimpleView(post.sharesCount)
             }
         }
     }
--- app/src/main/java/ru/netology/trialapplication/dto/Post.kt
+++ app/src/main/java/ru/netology/trialapplication/dto/Post.kt
@@ -1,6 +1,5 @@
 package ru.netology.trialapplication.dto
 
-
 data class Post(
         val id: Long,
         val author: String,
@@ -8,5 +7,72 @@ data class Post(
         val content: String,
         var likedByMe: Boolean = false,
         var likesCount: Int,
-        var sharesCount: Long
-)
+        var sharesCount: Int,
+        var viewsCount: Int
+) {
+        fun likesCountSimpleView(likesCount: Int): String {
+                var result: Int
+                var remainder: Int
+                var remaindersFirstChar: String
+
+                if (likesCount / 1000000 != 0 ) {
+                        result = likesCount / 1000000
+                        remainder = likesCount % 1000000
+                        if (remainder < 100000) {
+                                return "$result M"
+                        } else
+                                remaindersFirstChar = remainder.toString().take(1)
+                        return "$result.$remaindersFirstChar M"
+                }
+
+                if (likesCount / 10000 != 0 ) {
+                        result = likesCount / 1000
+                        return "$result K"
+                }
+
+                if (likesCount / 1000 != 0 ) {
+                        result = likesCount / 1000
+                        remainder = likesCount % 1000
+                        if (remainder < 100) {
+                                return "$result K"
+                        } else
+                        remaindersFirstChar = remainder.toString().take(1)
+                        return "$result.$remaindersFirstChar K"
+                }
+
+                return likesCount.toString()
+        }
+
+        fun sharesCountSimpleView(sharesCount: Int): String {
+                var result: Int
+                var remainder: Int
+                var remaindersFirstChar: String
+
+                if (sharesCount / 1000000 != 0 ) {
+                        result = sharesCount / 1000000
+                        remainder = sharesCount % 1000000
+                        if (remainder < 100000) {
+                                return "$result M"
+                        } else
+                                remaindersFirstChar = remainder.toString().take(1)
+                        return "$result.$remaindersFirstChar M"
+                }
+
+                if (sharesCount / 10000 != 0 ) {
+                        result = sharesCount / 1000
+                        return "$result K"
+                }
+
+                if (sharesCount / 1000 != 0 ) {
+                        result = sharesCount / 1000
+                        remainder = sharesCount % 1000
+                        if (remainder < 100) {
+                                return "$result K"
+                        } else
+                                remaindersFirstChar = remainder.toString().take(1)
+                        return "$result.$remaindersFirstChar K"
+                }
+
+                return sharesCount.toString()
+        }
+}
